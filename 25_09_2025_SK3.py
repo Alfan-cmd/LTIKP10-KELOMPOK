11bf7e9c800d3ae08bbce4bb1e33942be193a896
K7Unk <ExtacyLV@icloud.com>
2025-11-08 01:27:58 +0700

    Menambah Login

--- 25_09_2025_SK3.py
+++ 25_09_2025_SK3.py
@@ -70,3 +70,29 @@ def sapa():
         if "c" in pilihanuser:
             break
 sapa()
+
+def login(a):
+    jumlah = a
+    while True:
+        if jumlah == 0:
+            print(f"Salah {a} kali")
+            return 
+
+        user = input("\nMasukkan id: ")
+        password = input("Masukkan password: ")
+
+        if jumlah == 0:
+            print("Kesempatan anda sudah habis: ")
+            break
+
+        else:
+            if "FammRIOT" in user and "Fauzivalorant123" in password:
+                print("Selamat datang User!")
+                break
+            
+            else:
+                print("\nMasukkan kembali id dan password")
+                jumlah -=1
+                print(f"Sisa pengulangan {jumlah}")
+                
+login(3)
